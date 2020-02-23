514ecbf7a00eeb700d5207cdc3d699f90a79b83a
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-23 18:23:06 -0300

    utilizando o multiElementos

--- src/shared/PrimeiroComponente.js
+++ src/shared/PrimeiroComponente.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import MultiElementos from './MultiElementos';
 
 export default (props) => 
     <div>
         <h1>{props.valor}</h1>
+        <MultiElementos></MultiElementos>
     </div>
