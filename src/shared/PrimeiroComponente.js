376beddde30499091163e0a3bd1f6bb296026e90
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-24 19:18:08 -0300

    remoção dos comentários

--- src/shared/PrimeiroComponente.js
+++ src/shared/PrimeiroComponente.js
@@ -1,26 +1,7 @@
 import React from 'react';
-// import Pai from './Pai';
-import ComponenteClasse from './ComponenteClasse';
-// import Familia from './Familia';
-// import Membro from './Membro';
-// import FamiliaSilva from './FamiliaSilva';
-// import ComponenteComFuncao from './ComponenteComFuncao';
 
-
-
-export default (props) => 
+export default _ => 
     <div>
-        {/* <Familia sobrenome="Pereira">
-            <Membro nome="Andre"></Membro>
-            <Membro nome="Mariana"></Membro>
-        </Familia>
-        <Familia>
-            <Membro nome="Bia" sobrenome="Arruda"></Membro>
-            <Membro nome="Gustavo" sobrenome="Arruda"></Membro>
-        </Familia>
-        <FamiliaSilva sobrenome="Silva Pereira"></FamiliaSilva>
-        <ComponenteComFuncao></ComponenteComFuncao> */}
-        {/* <Pai></Pai> */}
-        <ComponenteClasse valor="Componente do tipo classe"></ComponenteClasse>
+
     </div>
 
