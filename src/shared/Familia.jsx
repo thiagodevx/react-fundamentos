82033508f90f45ceb92442883bbf714af4326142
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-23 23:54:19 -0300

    Utilizando o React.Children.map e o React.cloneElement

--- src/shared/Familia.jsx
+++ src/shared/Familia.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 
 export default props =>
     <div>
-        {props.children}
+        {React.Children.map(props.children, child => {
+            return React.cloneElement(child, {sobrenome: props.sobrenome})
+        })}
     </div>
