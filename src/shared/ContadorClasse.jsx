d0ee1a7b0fc199c97dc8a0b30565dea399adacec
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-24 20:10:33 -0300

    contador de classe refatorado

--- src/shared/ContadorClasse.jsx
+++ src/shared/ContadorClasse.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class ContadorClasse extends React.Component {
-    constructor() {
-        super()
-        this.state = {value:0}
+export default class ContadorClasse extends Component {
+
+    state = {value: 0}
+    incrementar = () => {
+        this.setState({value: this.state.value + 1})
     }
+
     render(){
-        return <button onClick={()=>this.setState({value: this.state.value+1})}>{this.state.value}</button>
+        return (
+        <button onClick={this.incrementar}>
+            {this.state.value}
+        </button>
+        )
     }
 }
