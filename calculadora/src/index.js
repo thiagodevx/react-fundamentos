3531d97b06d016c3ec8f4207da524ffbf32db2aa
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-24 21:48:52 -0300

    centralização da calculadora

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Calculator from './main/Calculator';
+import './index.css'
 
 ReactDOM.render(<Calculator></Calculator>, document.getElementById('root'));
 serviceWorker.unregister();
