bb0931250be4db827f9444cf4f7cde7901f45420
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-24 22:24:59 -0300

    css grid to organize the calculator

--- calculadora/src/main/Calculator.js
+++ calculadora/src/main/Calculator.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import Button from '../button/Button'
 import './Calculator.css'
 
 export default _ => {
 
     return (
         <div className="calculator">
-
+            <Button label={'AC'}></Button>
+            <Button label={'/'}></Button>
+            <Button label={'7'}></Button>
+            <Button label={'8'}></Button>
+            <Button label={'9'}></Button>
+            <Button label={'*'}></Button>
+            <Button label={'4'}></Button>
+            <Button label={'5'}></Button>
+            <Button label={'6'}></Button>
+            <Button label={'-'}></Button>
+            <Button label={'1'}></Button>
+            <Button label={'2'}></Button>
+            <Button label={'3'}></Button>
+            <Button label={'+'}></Button>
+            <Button label={'0'}></Button>
+            <Button label={'.'}></Button>
+            <Button label={'='}></Button>
         </div>
     )
 }
