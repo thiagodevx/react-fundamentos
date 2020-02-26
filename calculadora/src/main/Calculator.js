35c02ad01476b513c0f28de64d673761103c8bf9
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-26 01:17:33 -0300

    creating functions

--- calculadora/src/main/Calculator.js
+++ calculadora/src/main/Calculator.js
@@ -5,30 +5,40 @@ import Display from '../display/Display'
 
 export default _ => {
 
-    const buttonClicked = (buttonLabel) => {
-        alert('clicked on ' + buttonLabel)
+    let result = 0
+
+    const clearMemory = _ => {
+        alert('clear')
+    }
+
+    const setOperation = (operation) => {
+        alert(operation)
+    }
+
+    const addDigit = (digit) => {
+        alert(digit)
     }
 
     return (
         <div className="calculator">
-            <Display value='AAAAAAAA'></Display>
-            <Button clicked={buttonClicked} label='AC'></Button>
-            <Button clicked={buttonClicked} label='/'></Button>
-            <Button clicked={buttonClicked} label='7'></Button>
-            <Button clicked={buttonClicked} label='8'></Button>
-            <Button clicked={buttonClicked} label='9'></Button>
-            <Button clicked={buttonClicked} label='*'></Button>
-            <Button clicked={buttonClicked} label='4'></Button>
-            <Button clicked={buttonClicked} label='5'></Button>
-            <Button clicked={buttonClicked} label='6'></Button>
-            <Button clicked={buttonClicked} label='-'></Button>
-            <Button clicked={buttonClicked} label='1'></Button>
-            <Button clicked={buttonClicked} label='2'></Button>
-            <Button clicked={buttonClicked} label='3'></Button>
-            <Button clicked={buttonClicked} label='+'></Button>
-            <Button clicked={buttonClicked} label='0'></Button>
-            <Button clicked={buttonClicked} label='.'></Button>
-            <Button clicked={buttonClicked} label='='></Button>
+            <Display value={result}></Display>
+            <Button clicked={clearMemory} label='AC'></Button>
+            <Button clicked={setOperation} label='/'></Button>
+            <Button clicked={addDigit} label='7'></Button>
+            <Button clicked={addDigit} label='8'></Button>
+            <Button clicked={addDigit} label='9'></Button>
+            <Button clicked={setOperation} label='*'></Button>
+            <Button clicked={addDigit} label='4'></Button>
+            <Button clicked={addDigit} label='5'></Button>
+            <Button clicked={addDigit} label='6'></Button>
+            <Button clicked={setOperation} label='-'></Button>
+            <Button clicked={addDigit} label='1'></Button>
+            <Button clicked={addDigit} label='2'></Button>
+            <Button clicked={addDigit} label='3'></Button>
+            <Button clicked={setOperation} label='+'></Button>
+            <Button clicked={addDigit} label='0'></Button>
+            <Button clicked={addDigit} label='.'></Button>
+            <Button clicked={setOperation} label='='></Button>
         </div>
     )
 }
