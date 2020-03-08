d04291de86fd5ea0208e88e2340e8962e46aeeec
thiago.dev.x <thiago.dev.x@gmail.com>
2020-03-08 19:46:00 -0300

    Makes more sense that the done does not work

--- todo/backend/src/api/todo/todo.js
+++ todo/backend/src/api/todo/todo.js
@@ -3,7 +3,7 @@ const mongoose = restful.mongoose
 
 const todoSchema = new mongoose.Schema({
   description: { type: String, required: true },
-  done: { type: Boolean, required: true, default: false },
+  done: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 })
 
