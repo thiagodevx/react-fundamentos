059b8a633ae19328cbb91849a8ebdcaec37f1d3f
thiago.dev.x <thiago.dev.x@gmail.com>
2020-03-08 19:56:46 -0300

    permitindo o cors

--- todo/backend/src/config/server.js
+++ todo/backend/src/config/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('./cors')
+
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
-
+server.use(cors)
 const port = 3003
 server.listen(port, () => {
   console.log(`Backend is running on port ${port}.`)
