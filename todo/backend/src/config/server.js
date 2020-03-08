01abd1c2c877a1b0289ca6f0a124835a9d2277e3
thiago.dev.x <thiago.dev.x@gmail.com>
2020-03-08 19:16:01 -0300

    first steps configuring the server

---
+++ todo/backend/src/config/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+const port = 3003
+server.listen(port, () => {
+  console.log(`Backend is running on port ${port}`)
+})
