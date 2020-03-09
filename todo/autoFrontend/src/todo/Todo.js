18edb88d491a1ac40c6e78069868e66c13bcd825
thiago.dev.x <thiago.dev.x@gmail.com>
2020-03-08 21:46:02 -0300

    criação dos componentes base para o Todo

--- todo/autoFrontend/src/todo/Todo.js
+++ todo/autoFrontend/src/todo/Todo.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PageHeader from '../template/PageHeader'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
 
 export default _ => {
   return (
     <div>
       <PageHeader name='tarefas' small='cadastro'></PageHeader>
+      <TodoForm></TodoForm>
+      <TodoList></TodoList>
     </div>
   )
 }
---
+++ todo/autoFrontend/src/todo/TodoList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default _ => (
+  <div>
+    <h1>List</h1>
+  </div>
+)
