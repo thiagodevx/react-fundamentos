c0508aa83b2f894c09df51d8c9d8462d40854c61
thiago.dev.x <thiago.dev.x@gmail.com>
2020-03-08 21:13:55 -0300

    Criação do componente de menu

--- todo/autoFrontend/src/App.js
+++ todo/autoFrontend/src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './App.css'
 import Todo from './todo/Todo'
 import About from './about/About'
+import Menu from './template/Menu'
 
 export default _ => {
   return (
     <div className="container">
+      <Menu></Menu>
       <Todo></Todo>
       <About></About>
     </div>
