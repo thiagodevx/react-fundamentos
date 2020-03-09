8f295483a8ce3b74709782c81a21a459bfda645b
thiago.dev.x <thiago.dev.x@gmail.com>
2020-03-08 21:37:23 -0300

    using a switch

--- todo/autoFrontend/src/routes/Routes.js
+++ todo/autoFrontend/src/routes/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Todo from '../todo/Todo'
 import About from '../about/About'
 
 export default _ => (
-  <Router>
-    <Route exact path='/' component={Todo}></Route>
-    <Route path='/todos' component={Todo}></Route>
-    <Route path='/about' component={About}></Route>
-  </Router>
+  <BrowserRouter>
+    <Switch>
+      <Route path='/todos' component={Todo}></Route>
+      <Route path='/about' component={About}></Route>
+      <Route component={Todo}></Route>
+    </Switch>
+  </BrowserRouter>
 )
