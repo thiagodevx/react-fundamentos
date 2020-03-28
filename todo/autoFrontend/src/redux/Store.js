7bf99a345f4abccc68c7da9113fa7ff38eac8f84
thiagodevx <thiago.dev.x@gmail.com>
2020-03-28 16:58:30 -0300

    exclusão do basic actino e reducer, que foi construido apenas para teste

--- todo/autoFrontend/src/redux/Store.js
+++ todo/autoFrontend/src/redux/Store.js
@@ -1,9 +1,7 @@
 import { combineReducers, createStore } from 'redux'
-import BasicReducer from './BasicReducer'
 import TesteReducer from './teste/TesteReducer'
 
 const reducers = combineReducers({
-    state: BasicReducer,
     testeState: TesteReducer
 })
 export default createStore(reducers)
